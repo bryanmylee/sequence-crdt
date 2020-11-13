d18be6b84c442da5f592dc47db6954c12c106d53
Bryan Lee <bryanmylee@gmail.com>
2020-11-14 01:07:38 +0800

    test: use one file to scope entire test suite

--- test/model/crdt/element_test.c
+++ test/model/crdt/element_test.c
@@ -1,5 +1,10 @@
 #include <check.h>
-#include "./key_token_conv_test.c"
+#include <src/model/crdt/element.h>
+
+START_TEST(test_key_from_tokens) {
+  int result = key_from_tokens(3, 0, 2, 7);
+  ck_assert_uint_eq(60, result);
+} END_TEST
 
 Suite* element_suite(void) {
   Suite *s;

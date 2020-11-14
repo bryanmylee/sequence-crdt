54300c2db07e55847768d226b3eb9622d4edf32d
Bryan Lee <bryanmylee@gmail.com>
2020-11-15 06:23:10 +0800

    test(guid): add more tests for add_token

--- test/model/crdt/guid_test.c
+++ test/model/crdt/guid_test.c
@@ -34,6 +34,25 @@ START_TEST(test_guid_add_token) {
   ck_assert_int_eq(g.uids, expected.uids);
 } END_TEST
 
+START_TEST(test_guid_add_token_second_uid) {
+  guid g = {
+    .depth = 3,
+    .keys = keys_from_tokens(3, 0, 0, 0),
+    .uids = uids_from_tokens(3, 1, 1, 1),
+  };
+  token t = { .key = 1, .uid = 2 };
+  guid_add_token(&g, t);
+
+  guid expected = {
+    .depth = 4,
+    .keys = keys_from_tokens(4, 0, 0, 0, 1),
+    .uids = uids_from_tokens(4, 1, 1, 1, 2),
+  };
+  ck_assert_int_eq(g.depth, expected.depth);
+  ck_assert_int_eq(g.keys, expected.keys);
+  ck_assert_int_eq(g.uids, expected.uids);
+} END_TEST
+
 START_TEST(test_guid_copy) {
   unsigned long orig_keys = keys_from_tokens(3, 0, 1, 3);
   guid orig = {
@@ -228,6 +247,7 @@ Suite* element_suite(void) {
   tcase_add_test(tc_creating, test_keys_from_tokens);
   tcase_add_test(tc_creating, test_uids_from_tokens);
   tcase_add_test(tc_creating, test_guid_add_token);
+  tcase_add_test(tc_creating, test_guid_add_token_second_uid);
   tcase_add_test(tc_creating, test_guid_copy);
   suite_add_tcase(s, tc_creating);
 

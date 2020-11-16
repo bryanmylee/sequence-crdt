ea97aaa82eeb0eae3bda8d27f005337b7de3a8ce
Bryan Lee <bryanmylee@gmail.com>
2020-11-16 18:29:01 +0800

    test(seq): test finding index

--- test/model/crdt/sequence_test.c
+++ test/model/crdt/sequence_test.c
@@ -1,5 +1,8 @@
 #include <check.h>
 #include <stdlib.h>
+#include <model/arraylist.h>
+#include <model/crdt/element.h>
+#include <model/crdt/guid.h>
 #include <model/crdt/sequence.h>
 
 START_TEST(test_seq_token_between) {
@@ -136,12 +139,71 @@ START_TEST(test_seq_new_guid_between_same_key_different_uid) {
   free(result);
 } END_TEST
 
+START_TEST(test_seq_index_of) {
+  Sequence* s = seq_new();
+  // insert 8 Element pointers.
+  for (int i = 0; i < 8; i++) {
+    Element* e = element_new();
+    e->value = malloc(sizeof(int));
+    // give all elements a simple incremental guid.
+    // at depth 3, all 8 elements can fit under one node.
+    e->id = (Guid) {
+      .depth = 3,
+      .keys = keys_from_tokens(3, 0, 0, i),
+    };
+    *((int*) e->value) = i;
+    al_add(&s->elements, (void*) e);
+  }
+
+  Element to_find;
+  element_init(&to_find);
+  to_find.id = (Guid) {
+    .depth = 3,
+    .keys = keys_from_tokens(3, 0, 0, 5),
+  };
+  int index_of_to_find = seq_index_of_element_or_after(s, &to_find);
+  ck_assert_int_eq(index_of_to_find, 5);
+
+  seq_free(&s);
+} END_TEST
+
+START_TEST(test_seq_index_of_non_existent) {
+  Sequence* s = seq_new();
+  // insert 8 Element pointers.
+  for (int i = 0; i < 8; i++) {
+    Element* e = element_new();
+    e->value = malloc(sizeof(int));
+    // give all elements a simple incremental guid.
+    // at depth 3, all 8 elements can fit under one node.
+    e->id = (Guid) {
+      .depth = 3,
+      .keys = keys_from_tokens(3, 0, 0, i),
+    };
+    *((int*) e->value) = i;
+    al_add(&s->elements, (void*) e);
+  }
+
+  // should be at the very end of the sequence.
+  Element to_find;
+  element_init(&to_find);
+  to_find.id = (Guid) {
+    .depth = 3,
+    .keys = keys_from_tokens(3, 1, 0, 0),
+  };
+  int index_of_to_find = seq_index_of_element_or_after(s, &to_find);
+  ck_assert_int_eq(index_of_to_find, 8);
+
+  seq_free(&s);
+} END_TEST
+
 Suite* sequence_suite(void) {
   Suite *s;
   TCase *tc_guid;
+  TCase *tc_core;
 
   s = suite_create("sequence_suite");
   tc_guid = tcase_create("guid");
+  tc_core = tcase_create("core");
 
   tcase_add_test(tc_guid, test_seq_token_between);
   tcase_add_test(tc_guid, test_seq_token_between_no_space);
@@ -151,7 +213,11 @@ Suite* sequence_suite(void) {
   tcase_add_test(tc_guid, test_seq_new_guid_between_uncle_nephew);
   tcase_add_test(tc_guid, test_seq_new_guid_between_nephew_uncle);
   tcase_add_test(tc_guid, test_seq_new_guid_between_same_key_different_uid);
-  suite_add_tcase(s, tc_guid);
+  /* suite_add_tcase(s, tc_guid); */
+
+  tcase_add_test(tc_core, test_seq_index_of);
+  tcase_add_test(tc_core, test_seq_index_of_non_existent);
+  suite_add_tcase(s, tc_core);
 
   return s;
 }

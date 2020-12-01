81db050a2562eade54a34348b72cddf6d2877620
Bryan Lee <bryanmylee@gmail.com>
2020-12-01 22:43:36 +0800

    test: implement out of bounds get tests

--- tests/check_arraylist_get_set.c
+++ tests/check_arraylist_get_set.c
@@ -12,7 +12,19 @@ START_TEST(test_al_get) {
   for (int i = 0; i < 10; i++) {
     ck_assert_int_eq(elements[i], *(int*) al_get(al, i));
   }
+  al_free(&al);
 } END_TEST
+
+START_TEST(test_al_get_out_of_bounds) {
+  ArrayList* al = al_new(sizeof(int));
+  int elements[10] = { 4, 5, 12, 512, 23, 50, 123, 77, 23, 10812 };
+  for (int i = 0; i < 10; i++) {
+    al_add(al, &elements[i]);
+  }
+  ck_assert_ptr_null(al_get(al, -1));
+  ck_assert_ptr_null(al_get(al, 10));
+  al_free(&al);
+}
 
 Suite* arraylist_get_set_suite(void) {
   Suite *s;
@@ -30,6 +42,7 @@ Suite* arraylist_get_set_suite(void) {
 
   suite_add_tcase(s, tc_set);
 
+  tcase_add_test(tc_boundary, test_al_get_out_of_bounds);
   suite_add_tcase(s, tc_boundary);
 
   return s;

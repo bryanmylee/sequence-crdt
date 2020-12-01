0f17b19295a8df917c9e11a10429606281ade677
Bryan Lee <bryanmylee@gmail.com>
2020-12-02 02:59:43 +0800

    test(seq): increase coverage of testing
    Test inserting elements out of bounds and saving inserted pointer
    elements.

--- tests/check_sequence_insert_delete.c
+++ tests/check_sequence_insert_delete.c
@@ -3,29 +3,6 @@
 
 #include <sequence.h>
 
-START_TEST(test_seq_insert_value) {
-  Sequence *s = seq_new();
-  char *data = "this is a string";
-  int n = strlen(data);
-  for (int i = 0; i < n; i++) {
-    seq_insert_value(s, data[i], i);
-  }
-
-  // check that all elements are stored properly.
-  for (int i = 0; i < n; i++) {
-    Element *e = seq_get_element(s, i);
-    char c = e->data.value;
-    ck_assert_int_eq(c, data[i]);
-  }
-
-  // check that all elements are sorted by Guid.
-  for (unsigned int i = 1; i < s->elements.size; i++) {
-    Element *prev = al_get(&s->elements, i - 1);
-    Element *curr = al_get(&s->elements, i);
-    ck_assert_int_lt(guid_compare(&prev->id, &curr->id), 0);
-  }
-} END_TEST
-
 START_TEST(test_seq_insert) {
   Sequence *s = seq_new();
   char *data = "this is a string";
@@ -38,6 +15,56 @@ START_TEST(test_seq_insert) {
   for (int i = 0; i < n; i++) {
     Element *e = seq_get_element(s, i);
     char c = *((char*) e->data.ptr);
+    ck_assert_int_eq(c, data[i]);
+  }
+
+  // check that all elements are sorted by Guid.
+  for (unsigned int i = 1; i < s->elements.size; i++) {
+    Element *prev = al_get(&s->elements, i - 1);
+    Element *curr = al_get(&s->elements, i);
+    ck_assert_int_lt(guid_compare(&prev->id, &curr->id), 0);
+  }
+} END_TEST
+
+START_TEST(test_seq_insert_save) {
+  Sequence *s = seq_new();
+  char *data = "this is a string";
+  int n = strlen(data);
+  for (int i = 0; i < n; i++) {
+    Element e;
+    // insert and save the element inserted.
+    seq_insert_save(s, data + i, i, &e);
+    // check if the data pointer is the same.
+    ck_assert_ptr_eq(data + i, e.data.ptr);
+  }
+
+  // check that all elements are stored properly.
+  for (int i = 0; i < n; i++) {
+    Element *e = seq_get_element(s, i);
+    char c = *((char*) e->data.ptr);
+    ck_assert_int_eq(c, data[i]);
+  }
+
+  // check that all elements are sorted by Guid.
+  for (unsigned int i = 1; i < s->elements.size; i++) {
+    Element *prev = al_get(&s->elements, i - 1);
+    Element *curr = al_get(&s->elements, i);
+    ck_assert_int_lt(guid_compare(&prev->id, &curr->id), 0);
+  }
+} END_TEST
+
+START_TEST(test_seq_insert_value) {
+  Sequence *s = seq_new();
+  char *data = "this is a string";
+  int n = strlen(data);
+  for (int i = 0; i < n; i++) {
+    seq_insert_value(s, data[i], i);
+  }
+
+  // check that all elements are stored properly.
+  for (int i = 0; i < n; i++) {
+    Element *e = seq_get_element(s, i);
+    char c = e->data.value;
     ck_assert_int_eq(c, data[i]);
   }
 
@@ -154,19 +181,38 @@ START_TEST(test_seq_remote_delete) {
   }
 } END_TEST
 
+START_TEST(test_seq_insert_out_of_bounds) {
+  Sequence *s = seq_new();
+  char *data = "this is a string";
+  int n = strlen(data);
+  for (int i = 0; i < n; i++) {
+    seq_insert(s, data + i, i);
+  }
+
+  // n + 1 is out of bounds, but n is in bounds.
+  ck_assert(seq_insert_value(s, 'a', n + 1) == false);
+  ck_assert(seq_insert_value(s, 'a', n) == true);
+} END_TEST
+
 Suite *sequence_insert_delete_suite(void) {
   Suite *s;
   TCase *tc_core;
+  TCase *tc_boundary;
 
   s = suite_create("check_sequence_insert_delete");
   tc_core = tcase_create("core");
+  tc_boundary = tcase_create("boundary");
 
-  tcase_add_test(tc_core, test_seq_insert_value);
   tcase_add_test(tc_core, test_seq_insert);
+  tcase_add_test(tc_core, test_seq_insert_save);
+  tcase_add_test(tc_core, test_seq_insert_value);
   tcase_add_test(tc_core, test_seq_delete);
   tcase_add_test(tc_core, test_seq_remote_insert);
   tcase_add_test(tc_core, test_seq_remote_delete);
   suite_add_tcase(s, tc_core);
+
+  tcase_add_test(tc_boundary, test_seq_insert_out_of_bounds);
+  suite_add_tcase(s, tc_boundary);
 
   return s;
 }

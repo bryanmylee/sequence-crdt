2457f834f70db5ad223aeaa187a14149f63f2e76
Bryan Lee <bryanmylee@gmail.com>
2020-12-02 10:51:44 +0800

    feat(element): keep track of whether element is value or ref type

--- tests/check_element.c
+++ tests/check_element.c
@@ -10,13 +10,57 @@ START_TEST(test_element_new) {
   element_free(&e);
 } END_TEST
 
-START_TEST(test_element_free_ptr) {
+START_TEST(test_element_free) {
   Element *e = element_new();
-  e->data.ptr = malloc(sizeof(int));
-  *(int *) e->data.ptr = 50;
-  ck_assert_int_eq(50, *(int *) e->data.ptr);
-  element_free_ptr(&e);
+  int *x = malloc(sizeof(int));
+  *x = 5;
+  element_set_ptr(e, x);
+  element_free(&e);
   ck_assert_ptr_null(e);
+} END_TEST
+
+START_TEST(test_element_set_ptr) {
+  Element *e = element_new();
+  int *x = malloc(sizeof(int));
+  *x = 5;
+
+  element_set_ptr(e, x);
+
+  ck_assert_int_eq(5, *(int *) e->data.ptr);
+  ck_assert_int_eq(EREF, e->type);
+
+  element_free(&e);
+}
+
+START_TEST(test_element_get_ptr) {
+  Element *e = element_new();
+  int *x = malloc(sizeof(int));
+  *x = 5;
+  element_set_ptr(e, x);
+
+  ck_assert_int_eq(5, *(int *) element_get_ptr(e));
+
+  element_free(&e);
+} END_TEST
+
+START_TEST(test_element_set_value) {
+  Element *e = element_new();
+
+  element_set_value(e, 5);
+
+  ck_assert_int_eq(5, e->data.value);
+  ck_assert_int_eq(EVALUE, e->type);
+
+  element_free(&e);
+}
+
+START_TEST(test_element_get_value) {
+  Element *e = element_new();
+  element_set_value(e, 5);
+
+  ck_assert_int_eq(5, element_get_value(e));
+
+  element_free(&e);
 } END_TEST
 
 Suite *element_suite(void) {
@@ -27,7 +71,11 @@ Suite *element_suite(void) {
   tc_core = tcase_create("core");
 
   tcase_add_test(tc_core, test_element_new);
-  tcase_add_test(tc_core, test_element_free_ptr);
+  tcase_add_test(tc_core, test_element_free);
+  tcase_add_test(tc_core, test_element_get_ptr);
+  tcase_add_test(tc_core, test_element_set_ptr);
+  tcase_add_test(tc_core, test_element_get_value);
+  tcase_add_test(tc_core, test_element_set_value);
   suite_add_tcase(s, tc_core);
 
   return s;
